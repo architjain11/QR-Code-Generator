2f5e1f1a6cf9478c68615fe059073dd894ab8331
architjain11 <architjain@gmail.com>
2022-06-02 02:06:31 +0530

    initial code commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import pyqrcode
+import png
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    message = 'Built by Archit'
+    return render_template("index.html", message=message)
+
+@app.route("/newQR")
+def newQR():
+    message = request.args.get('message')
+    d = pyqrcode.create(message)
+    d.png('output.png', scale=6)
+    return render_template("index.html", message=message)
